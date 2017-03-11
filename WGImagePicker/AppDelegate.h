2c4e0cfe31975a9d35edb12babc25784bf92f4e4
Prashant Raju <prashant.raju@me.com>
2017-03-11 14:01:44 -0800

    Initial Commit

---
+++ WGImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGImagePicker
+//
+//  Created by Prashant Raju on 3/11/17.
+//  Copyright © 2017 Prashant Raju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGImagePicker
+//
+//  Created by Prashant Raju on 3/11/17.
+//  Copyright © 2017 Prashant Raju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
